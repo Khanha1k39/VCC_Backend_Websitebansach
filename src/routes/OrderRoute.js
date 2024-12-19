cbd75f748aa2b0a11f764e417a61762a206a452d
duongkhanhb1k39 <duongkhanhb1k39@gmail.com>
2024-12-19 10:22:39 +0700

    update order service

--- src/routes/OrderRoute.js
+++ src/routes/OrderRoute.js
@@ -3,5 +3,6 @@ const orderController = require("../controllers/OrderController");
 const { authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 router.post("/create", orderController.createOrder);
+router.post("/check", orderController.checkQuantityStock);
 
 module.exports = router;
--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,19 +1,53 @@
 const Book = require("../models/BookModel");
 const bcrypt = require("bcrypt");
 const User = require("../models/UserModel");
+const { sendEmailCreateOrder } = require("./EmailService");
 const createOrder = async (newOrder) => {
   return new Promise(async (res, rej) => {
-    // const { name, image, type, price, countInStock, rating, description } =
-    //   newProduct;
-    return res({ hihi });
-
     try {
-      const createBook = await Book.create(newProduct);
-      if (createBook) {
-        res({ status: "OK", message: "SUCCESS", data: createBook });
-      }
+      const { orderItems } = newOrder;
+      const checkStockPromises = orderItems.map((item) =>
+        Book.findById(item.product).then((product) => {
+          if (!product) {
+            throw new Error(`Sản phẩm ${item.name} không tồn tại trong kho.`);
+          }
+          if (product.quantity_available < item.amount) {
+            throw new Error(
+              `Sản phẩm ${item.name} chỉ còn ${product.quantity_available} sản phẩm, không đủ số lượng yêu cầu.`
+            );
+          }
+        })
+      );
+      const result = await Promise.all(checkStockPromises);
+      console.log(result);
+
+      res("ok");
     } catch (error) {
-      console.log(error);
+      rej(error);
+    }
+  });
+};
+
+const checkQuantityStock = async (newOrder) => {
+  return new Promise(async (res, rej) => {
+    try {
+      const { orderItems } = newOrder;
+      const checkStockPromises = orderItems.map((item) =>
+        Book.findById(item.product).then((product) => {
+          if (!product) {
+            throw new Error(`Sản phẩm ${item.name} không tồn tại trong kho.`);
+          }
+          if (product.quantity_available < item.amount) {
+            throw new Error(
+              `Sản phẩm ${item.name} chỉ còn ${product.quantity_available} sản phẩm, không đủ số lượng yêu cầu.`
+            );
+          }
+        })
+      );
+      await Promise.all(checkStockPromises);
+      res({ status: "OK", message: "Success" });
+    } catch (error) {
+      console.log("errpr rec", error);
       rej(error);
     }
   });
@@ -21,4 +55,5 @@ const createOrder = async (newOrder) => {
 
 module.exports = {
   createOrder,
+  checkQuantityStock,
 };
