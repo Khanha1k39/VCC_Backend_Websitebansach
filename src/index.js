64b435316beea96f8fd7c5e6d830739c4933e5e7
khanhduong <duongkhanhb1k39@gmail.com>
2025-04-03 01:24:49 +0700

    cors

--- src/index.js
+++ src/index.js
@@ -10,12 +10,7 @@ const userName = process.env.DB_USER_NAME;
 const password = process.env.DB_PW;
 const uri = `mongodb+srv://${userName}:${password}@cluster0.jmoxqb2.mongodb.net/vcc?retryWrites=true&w=majority&appName=Cluster0`;
 const cookieParser = require("cookie-parser");
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 app.options("*", cors());
 
 app.use(bodyParse.json());
@@ -30,6 +25,7 @@ mongoose
     console.log("faile to conect db");
   });
 const { initRedis } = require("./dbs/redis");
+const { corsOptions } = require("./config/cors");
 initRedis();
 
 app.listen(port, () => {
